fb6e6ff4b925782470f995e1d324414154ec8709
Ébano Penha Andrello Lopes <ebano@onbi.com.br>
2017-08-07 15:16:15 -0300

    Adjusting property name to match API

--- lib/Models/Purchase.php
+++ lib/Models/Purchase.php
@@ -30,7 +30,7 @@ class Purchase extends SignifydModel
 
     public $products; // array
     public $shipments; // array
-    public $discountsCodes; // array
+    public $discountCodes; // array
 
     public function __construct()
     {
